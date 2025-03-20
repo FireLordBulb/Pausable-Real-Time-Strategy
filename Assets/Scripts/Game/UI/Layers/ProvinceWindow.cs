320c94cfcbff82d62d00496702a099202ac9dbb0
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-03-20 16:25:59 +0100

    Made ProvinceWindow show info about the province.

--- Assets/Scripts/Game/UI/Layers/ProvinceWindow.cs
+++ Assets/Scripts/Game/UI/Layers/ProvinceWindow.cs
@@ -1,10 +1,22 @@
+using System.Text;
+using TMPro;
+using UnityEngine;
+
 public class ProvinceWindow : UILayer {
+	[SerializeField] private TextMeshProUGUI color;
+	[SerializeField] private TextMeshProUGUI neighbors;
+	
 	private Province province;
 	private bool isDone;
 	public override void OnBegin(bool isFirstTime){
 		base.OnBegin(isFirstTime);
 		province = Stack.SelectedProvince;
-		gameObject.name = province.gameObject.name;
+		color.text = $"Color: {province.gameObject.name}";
+		StringBuilder neighborsString = new("Neighbors:");
+		foreach (ProvinceLink provinceLink in province.Links){
+			neighborsString.Append($"\n{provinceLink.Target.gameObject.name}");
+		}
+		neighbors.text = neighborsString.ToString();
 		province.OnSelect();
 	}
 	public override void OnUpdate(){
