b6f09edb288d93c9c73f811686167ad2f4f1b884
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-03-25 14:16:19 +0100

    Made ProvinceWindow display terrain.

--- Assets/Scripts/Game/UI/Layers/ProvinceWindow.cs
+++ Assets/Scripts/Game/UI/Layers/ProvinceWindow.cs
@@ -1,10 +1,13 @@
 using System.Text;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProvinceWindow : UILayer {
 	[SerializeField] private TextMeshProUGUI color;
+	[SerializeField] private TextMeshProUGUI terrain;
 	[SerializeField] private TextMeshProUGUI neighbors;
+	[SerializeField] private Image terrainImage;
 	
 	private Province province;
 	private bool isDone;
@@ -12,6 +15,11 @@ public class ProvinceWindow : UILayer {
 		base.OnBegin(isFirstTime);
 		province = Stack.SelectedProvince;
 		color.text = $"Color: {province.gameObject.name}";
+		if (province.Terrain != null){
+			terrain.text = $"Terrain: {province.Terrain.Name}";
+			Texture2D texture = (Texture2D)province.Terrain.Material.mainTexture;
+			terrainImage.overrideSprite = Sprite.Create(texture, new Rect(Vector2.zero, new Vector2(texture.width, texture.height)), Vector2.zero);
+		}
 		StringBuilder neighborsString = new("Neighbors:");
 		foreach (ProvinceLink provinceLink in province.Links){
 			neighborsString.Append($"\n{provinceLink.Target.gameObject.name}");
