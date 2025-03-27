fb833fb9d2df1cc77629ce0d3c154e447cd763e8
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-03-27 17:14:22 +0100

    Added Calendar with pausable real-time system.

---
+++ Assets/Scripts/Game/Simulation/Calendar.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Calendar : MonoBehaviour {
+	public static Calendar Instance;
+	
+	private const float NoProgress = 0;
+	private const float FullProgress = 1;
+	
+	[SerializeField] public float[] speedTimeSteps;
+	[SerializeField] public int startingSpeed;
+	[SerializeField] private Date startDate;
+	
+	private Date currentDate;
+	private float speed;
+	private float tickProgress;
+
+	private int speedIndex;
+	public int SpeedIndex {
+		get => speedIndex;
+		set {
+			speedIndex = value;
+			speed = 1/speedTimeSteps[startingSpeed];
+		}
+	}
+	public bool IsPaused {get; set;}
+	
+	public UnityEvent OnDayTick => currentDate.OnDayTick;
+	public UnityEvent OnMonthTick => currentDate.OnMonthTick;
+	public UnityEvent OnYearTick => currentDate.OnYearTick;
+	
+	private void Awake(){
+		if (Instance != null){
+			Destroy(gameObject);
+		}
+		Instance = this;
+		IsPaused = false;
+		currentDate = new Date(startDate);
+		tickProgress = NoProgress;
+		SpeedIndex = startingSpeed;
+	}
+	private void Update(){
+		if (IsPaused){
+			return;
+		}
+		tickProgress += Time.deltaTime*speed;
+		if (tickProgress < FullProgress){
+			return;
+		}
+		tickProgress = NoProgress;
+		currentDate.ToNextDay();
+	}
+
+	public void ChangeSpeed(int change){
+		SpeedIndex = Mathf.Clamp(SpeedIndex+change, 0, speedTimeSteps.Length-1);
+	}
+}
---
+++ Assets/Scripts/Game/Simulation/Date.cs
@@ -0,0 +1,74 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public struct Date {
+	// 0-indexed months means Jan is 0 and Feb is 1.
+	private static readonly int February = 1;
+	private static readonly (string name, int days)[] Months = {
+		("January"  , 31), ("February", 28), ("March"   , 31), ("April"   , 30),
+		("May"      , 31), ("June"    , 30), ("July"    , 31), ("August"  , 31),
+		("September", 30), ("October" , 31), ("November", 30), ("December", 31)
+	};
+	
+	public int year;
+	public int month;
+	public int day;
+
+	public readonly UnityEvent OnDayTick;
+	public readonly UnityEvent OnMonthTick;
+	public readonly UnityEvent OnYearTick;
+
+	public Date(Date date){
+		year = date.year;
+		month = date.month;
+		day = date.day;
+		OnDayTick = new UnityEvent();
+		OnMonthTick = new UnityEvent();
+		OnYearTick = new UnityEvent();
+		Validate();
+	}
+	private void Validate(){
+		while (MonthLength() < day){
+			day -= MonthLength();
+			month++;
+			while (Months.Length <= month){
+				month -= Months.Length;
+				year++;
+			}
+		}
+	}
+	
+	public void ToNextDay(){
+		day++;
+		OnDayTick.Invoke();
+		if (MonthLength() >= day){
+			Debug.Log(this);
+			return;
+		}
+		day = 1;
+		month++;
+		OnMonthTick.Invoke();
+		if (Months.Length > month){
+			Debug.Log(this);
+			return;
+		}
+		month = 0;
+		year++;
+		OnYearTick.Invoke();
+		Debug.Log(this);
+	}
+	
+	public int MonthLength(){
+		// Leap year calculation is just a bunch of magic numbers. 97 leap years per 400 years.
+		if (month == February && year%4 == 0 && (year%100 != 0 || year%400 == 0)){
+			return Months[month].days+1;
+		}
+		return Months[month].days;
+	}
+	
+	public override string ToString(){
+		return $"{day} {Months[month].name} {year}";
+	}
+}
