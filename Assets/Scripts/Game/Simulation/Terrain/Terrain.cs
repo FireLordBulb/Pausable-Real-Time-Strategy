08a51b9330927dbaf175132325a8328d05514afe
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-04-02 14:51:49 +0200

    Added MoveSpeedModifiers to terrains

--- Assets/Scripts/Game/Simulation/Terrain/Terrain.cs
+++ Assets/Scripts/Game/Simulation/Terrain/Terrain.cs
@@ -6,10 +6,12 @@ namespace Simulation {
 		[SerializeField] private new string name;
 		[SerializeField] private Material material;
 		[SerializeField] private float developmentModifier;
+		[SerializeField] private float moveSpeedModifier;
 		[SerializeField] private float defenderAdvantage;
 		public string Name => name;
 		public Material Material => material;
 		public float DevelopmentModifier => developmentModifier;
+		public float MoveSpeedModifier => moveSpeedModifier;
 		public float DefenderAdvantage => defenderAdvantage;
 	}
 }
