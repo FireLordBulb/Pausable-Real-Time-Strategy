a12913e7e7e0551299accb1037fc6e95b892e425
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-04-03 10:52:29 +0200

    Made message saying if a unit is buildable or not refresh when needed.

--- Assets/Scripts/Game/Player/UILayers/WarMenu.cs
+++ Assets/Scripts/Game/Player/UILayers/WarMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Player {
-	public class WarMenu : UILayer {
+	public class WarMenu : UILayer, IRefreshable {
 		[SerializeField] private MilitaryUnitButton militaryUnitButton;
 		[SerializeField] private RectTransform army;
 		[SerializeField] private RectTransform navy;
@@ -18,6 +18,7 @@ namespace Player {
 		
 		private void Awake(){
 			close.onClick.AddListener(() => isDone = true);
+			Calendar.Instance.OnMonthTick.AddListener(Refresh);
 		}
 		public override void OnBegin(bool isFirstTime){
 			if (!isFirstTime){
@@ -56,10 +57,22 @@ namespace Player {
 			selectedButton = button;
 			selectedRegimentType = unitType as RegimentType;
 			selectedShipType = unitType as ShipType;
-			selectedButton.Message.text = unitType.CanBeBuiltBy(Player) ? $"<color=green>Can be {unitType.CreatedVerb}</color>" : "<color=red>Cannot afford!</color>";
+			Refresh();
 			selectedButton.ShowInfoBox();
 			EventSystem.current.SetSelectedGameObject(selectedButton.gameObject);
 		}
+		
+		public void Refresh(){
+			if (selectedRegimentType != null){
+				Refresh(selectedRegimentType);
+			} else if (selectedShipType != null){
+				Refresh(selectedShipType);
+			}
+		}
+		private void Refresh<T>(UnitType<T> unitType) where T : Branch {
+			selectedButton.Message.text = unitType.CanBeBuiltBy(Player) ? $"<color=green>Can be {unitType.CreatedVerb}</color>" : "<color=red>Cannot afford!</color>";
+		}
+
 		public override Component OnSelectableClicked(Component clickedSelectable, bool isRightClick){
 			if (isRightClick){
 				isDone = true;
@@ -68,12 +81,18 @@ namespace Player {
 				clickedProvince ??= (clickedSelectable as Regiment)?.Location.Province;
 				if (selectedRegimentType != null){
 					Player.TryStartBuildingArmy(selectedRegimentType, clickedProvince);
+					Refresh();
 				} else if (selectedShipType != null){
 					// TODO: TryStartBuildingNavy()
+					Refresh();
 				}
 			}
 			return UI.Selected;
-		}	
+		}
+		public override void OnEnd(){
+			Calendar.Instance.OnMonthTick.RemoveListener(Refresh);
+			base.OnEnd();
+		}
 		public override bool IsDone(){
 			base.IsDone();
 			return isDone;
