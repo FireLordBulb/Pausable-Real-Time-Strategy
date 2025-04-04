944599431e2700b0fa0397891b36861e2aea417b
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-04-04 14:42:43 +0200

    Added basic dice roll 1v1 battles with stack wipe as only result.

--- Assets/Scripts/Game/Simulation/Military/Location.cs
+++ Assets/Scripts/Game/Simulation/Military/Location.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Simulation.Military {
 	public abstract class Location<T> where T : Branch {
 		private readonly List<Unit<T>> units = new();
+
+		private bool battleIsOngoing;
+		// TODO: Allow for multiple units on each side.
+		private Unit<T> defendingUnit;
+		private Unit<T> attackingUnit;
 		
 		public abstract string Name {get;}
 		public virtual Province SearchTargetProvince => Province;
@@ -11,12 +18,52 @@ namespace Simulation.Military {
 		public abstract Vector3 WorldPosition {get;}
 
 		public IEnumerable<Unit<T>> Units => units;
+		public bool IsBattleOngoing => defendingUnit != null;
 		
 		public void Add(Unit<T> unit){
+			if (battleIsOngoing){
+				throw new NotImplementedException("Reinforcing battles hasn't been implemented yet!");
+			}
+			if (0 < units.Count){
+				Unit<T> alreadyPresentUnit = units[0];
+				if (alreadyPresentUnit.Owner != unit.Owner){
+					defendingUnit = alreadyPresentUnit;
+					attackingUnit = unit;
+					Calendar.Instance.OnDayTick.AddListener(BattleTick);
+				}
+			}
 			units.Add(unit);
 		}
 		public void Remove(Unit<T> unit){
 			units.Remove(unit);
+			if (battleIsOngoing){
+				EndBattle();
+			}
+		}
+		
+		private void BattleTick(){
+			BattleResult result = DefendBattle();
+			if (result == BattleResult.Ongoing){
+				return;
+			}
+			(result == BattleResult.AttackerWon ? defendingUnit : attackingUnit).StackWipe();
+			EndBattle();
+		}
+		private BattleResult DefendBattle(){
+			int diceRoll = Random.Range(0, 6);
+			if (diceRoll < 3){
+				return BattleResult.Ongoing;
+			}
+			if (diceRoll == 3){
+				return BattleResult.AttackerWon;
+			}
+			return BattleResult.DefenderWon;
+		}
+
+		private void EndBattle(){
+			defendingUnit = null;
+			attackingUnit = null;
+			Calendar.Instance.OnDayTick.RemoveListener(BattleTick);
 		}
 		
 		public virtual void AdjustPath(List<ProvinceLink> path){}
