7167f022201ed7697c1028529341d8f71188a2c5
Vilgot Ottestig <vilgot.ottestig@gmail.com>
2025-04-05 20:08:13 +0200

    Added combat width to Terrains

--- Assets/Scripts/Game/Simulation/Terrain/Terrain.cs
+++ Assets/Scripts/Game/Simulation/Terrain/Terrain.cs
@@ -8,10 +8,12 @@ namespace Simulation {
 		[SerializeField] private float developmentModifier;
 		[SerializeField] private float moveSpeedModifier;
 		[SerializeField] private float defenderAdvantage;
+		[SerializeField] private int combatWidth;
 		public string Name => name;
 		public Material Material => material;
 		public float DevelopmentModifier => developmentModifier;
 		public float MoveSpeedModifier => moveSpeedModifier;
 		public float DefenderAdvantage => defenderAdvantage;
+		public int CombatWidth => combatWidth;
 	}
 }
