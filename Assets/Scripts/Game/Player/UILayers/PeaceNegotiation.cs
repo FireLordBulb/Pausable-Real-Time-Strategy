a2c06e53e909cb3dbdeace54d0c41b4b8e7bf5d7
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-04-09 11:03:12 +0200

    Adde ability to demand or give gold in peace deals.

--- Assets/Scripts/Game/Player/UILayers/PeaceNegotiation.cs
+++ Assets/Scripts/Game/Player/UILayers/PeaceNegotiation.cs
@@ -20,15 +20,21 @@ namespace Player {
 		[SerializeField] private Button payLess;
 		[SerializeField] private Button payMore;
 		[SerializeField] private Button sendOffer;
+		[Header("Transfer Change Amounts")]
+		[SerializeField] private float baseChange;
+		[SerializeField] private float shiftMultiplier;
+		[SerializeField] private float ctrlMultiplier;
 		
 		private PeaceTreaty treaty;
 		private Country player;
 		private Country enemy;
+		private float otherCountryGoldTransfer;
 		private bool isDone;
 		
 		private void Awake(){
 			player = Player;
 			UI.Selected.OnDeselect();
+			
 			makeDemands.onClick.AddListener(() => {
 				SetWinner(true);
 			});
@@ -38,17 +44,28 @@ namespace Player {
 				foreach (Land land in treaty.AnnexedLands){
 					land.Province.OnDeselect();
 				}
-				treatyTerms.text = "- White Peace";
+				UpdateTreatyTerms();
 			});
 			giveConcessions.onClick.AddListener(() => {
 				SetWinner(false);
 			});
+			
+			payLess.onClick.AddListener(() => {
+				treaty.GoldTransfer -= baseChange;
+				Refresh();
+			});
+			payMore.onClick.AddListener(() => {
+				treaty.GoldTransfer += baseChange;
+				Refresh();
+			});
+			
 			sendOffer.onClick.AddListener(() => {
 				Player.EndWar(enemy, treaty);
 				isDone = true;
 				UI.Deselect();
 				LayerBelow.OnEnd();
 			});
+
 			Calendar.Instance.OnMonthTick.AddListener(Refresh);
 		}
 		
@@ -59,8 +76,11 @@ namespace Player {
 			enemyPanel.SetCountry(enemy, Close);
 			Refresh();
 		}
-		
-		public void Refresh(){}
+
+		public void Refresh(){
+			UpdateGoldTransfer();
+			UpdateTreatyTerms();
+		}
 		public override ISelectable OnSelectableClicked(ISelectable clickedSelectable, bool isRightClick){
 			Province clickedProvince = clickedSelectable as Province ?? (clickedSelectable as IUnit)?.Province;
 			if (clickedProvince == null || clickedProvince.IsSea){
@@ -96,7 +116,11 @@ namespace Player {
 		
 		private void SetWinner(bool isPlayer){
 			treaty.IsWhitePeace = false;
-			treaty.DidTreatyInitiatorWin = isPlayer;
+			if (treaty.DidTreatyInitiatorWin != isPlayer){
+				treaty.DidTreatyInitiatorWin = isPlayer;
+				(treaty.GoldTransfer, otherCountryGoldTransfer) = (otherCountryGoldTransfer, treaty.GoldTransfer);
+				UpdateGoldTransfer();
+			}
 			SetSelectedButton(isPlayer ? makeDemands : giveConcessions);
 			foreach (Land land in treaty.AnnexedLands){
 				if (land.Owner == treaty.Loser){
@@ -105,7 +129,23 @@ namespace Player {
 					land.Province.OnDeselect();
 				}
 			}
+			reparationsRow.SetActive(true);
 			UpdateTreatyTerms();
+		}
+		private void UpdateGoldTransfer(){
+			if (treaty.GoldTransfer <= 0){
+				treaty.GoldTransfer = 0;
+				payLess.interactable = false;
+			} else {
+				payLess.interactable = true;
+			}
+			if (treaty.GoldTransfer >= treaty.Loser.Gold){
+				treaty.GoldTransfer = treaty.Loser.Gold;
+				payMore.interactable = false;
+			} else {
+				payMore.interactable = true;
+			}
+			goldTransfer.text = $"{Format.FormatLargeNumber(treaty.GoldTransfer, Format.FiveDigits)}<color=yellow>G</color>";
 		}
 		private void SetSelectedButton(Button button){
 			makeDemands.interactable = true;
@@ -114,6 +154,11 @@ namespace Player {
 			button.interactable = false;
 		}
 		private void UpdateTreatyTerms(){
+			if (treaty.IsWhitePeace){
+				treatyTerms.text = "- White Peace";
+				reparationsRow.SetActive(false);
+				return;
+			}
 			StringBuilder builder = new();
 			builder.Append($"- {treaty.Loser} concedes defeat");
 			int annexedProvinceCount = treaty.AnnexedLands.Count(land => land.Owner == treaty.Loser);
@@ -121,9 +166,10 @@ namespace Player {
 				builder.Append($"\n- {treaty.Winner} annexes {annexedProvinceCount} province{(annexedProvinceCount == 1 ? "" : 's')} from {treaty.Loser}");
 			}
 			if (0 < treaty.GoldTransfer){
-				builder.Append($"\n- {treaty.Loser} pays {treaty.GoldTransfer} gold in reparations to {treaty.Winner}");
+				builder.Append($"\n- {treaty.Loser} pays {Format.FormatLargeNumber(treaty.GoldTransfer, Format.FiveDigits)} gold in reparations to {treaty.Winner}");
 			}
 			treatyTerms.text = builder.ToString();
+			reparationsRow.SetActive(true);
 		}
 		
 		public override void OnEnd(){
