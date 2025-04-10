a6d6b749abb656d9512c5cdc5e75a29245781356
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-04-10 12:14:00 +0200

    Fixed TargetLand and TargetSea incorrectly giving the Land/Sea of the Source province.

--- Assets/Scripts/Game/Simulation/Graph/LandLink.cs
+++ Assets/Scripts/Game/Simulation/Graph/LandLink.cs
@@ -1,7 +1,7 @@
 namespace Simulation {
 	public class LandLink : ProvinceLink {
 		public Land SourceLand => Source.Land;
-		public Land TargetLand => Source.Land;
+		public Land TargetLand => Target.Land;
 		public LandLink(Province source, Province target, int segmentIndex) : base(source, target, segmentIndex){}
 	}
 }
--- Assets/Scripts/Game/Simulation/Graph/SeaLink.cs
+++ Assets/Scripts/Game/Simulation/Graph/SeaLink.cs
@@ -1,7 +1,7 @@
 namespace Simulation {
 	public class SeaLink : ProvinceLink {
 		public Sea SourceSea => Source.Sea;
-		public Sea TargetSea => Source.Sea;
+		public Sea TargetSea => Target.Sea;
 		public SeaLink(Province source, Province target, int segmentIndex) : base(source, target, segmentIndex){}
 	}
 }
