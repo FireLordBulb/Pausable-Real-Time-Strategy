5d7394cd9123cf46ebe3b159b46a7e15585a6bde
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-04-11 15:37:57 +0200

    Regrouped methods in Country.cs

--- Assets/Scripts/Game/Simulation/Country.cs
+++ Assets/Scripts/Game/Simulation/Country.cs
@@ -79,55 +79,11 @@ namespace Simulation {
 		}
 		#endregion
 		
-		#region Regenerating the Border Mesh
-		private void RegenerateBorder(){
-			DestroyImmediate(borderMeshFilter.sharedMesh);
-			if (provinces.Count == 0){
-				return;
-			}
-			MeshData borderMeshData = new($"{gameObject.name}BorderMesh");
-			HashSet<Province> unsearchedProvinces = new(provinces.Select(land => land.Province));
-			CountryBorder.Generate(borderMeshData, this, unsearchedProvinces, borderHalfWidth);
-			borderMeshFilter.sharedMesh = borderMeshData.ToMesh();
-			wasBorderChanged = false;
-		}
-		private void Update(){
-			if (wasBorderChanged){
-				RegenerateBorder();
-			}
-		}
-		#endregion
-		
 		#region Changing Resource Amounts
 		public void GainResources(float gold, int manpower, int sailors){
 			Gold += gold;
 			Manpower += manpower;
 			Sailors += sailors;
-		}
-		#endregion
-		
-		#region Annexing and Occupying Land
-		internal void GainProvince(Land province){
-			if (province.Occupier == this){
-				province.Unoccupy();
-			}
-			ChangeProvinceCount(provinces.Add(province), +1);
-		}
-		internal void LoseProvince(Land province){
-			ChangeProvinceCount(provinces.Remove(province), -1);
-		}
-		private void ChangeProvinceCount(bool wasChanged, int change){
-			if (!wasChanged){
-				return;
-			}
-			ProvinceCount += change;
-			wasBorderChanged = true;
-		}
-		internal void GainOccupation(Land province){
-			occupations.Add(province);
-		}
-		internal void LoseOccupation(Land province){
-			occupations.Remove(province);
 		}
 		#endregion
 		
@@ -216,13 +172,49 @@ namespace Simulation {
 		}
 		#endregion
 		
-		#region Getter Methods
-		public DiplomaticStatus GetDiplomaticStatus(Country other){
-			return map.GetDiplomaticStatus(this, other);
+		#region Annexing and Occupying Land
+		internal void GainProvince(Land province){
+			if (province.Occupier == this){
+				province.Unoccupy();
+			}
+			ChangeProvinceCount(provinces.Add(province), +1);
 		}
-		public PeaceTreaty NewPeaceTreaty(Country recipient){
-			return new PeaceTreaty(this, recipient, truceData);
+		internal void LoseProvince(Land province){
+			ChangeProvinceCount(provinces.Remove(province), -1);
 		}
+		private void ChangeProvinceCount(bool wasChanged, int change){
+			if (!wasChanged){
+				return;
+			}
+			ProvinceCount += change;
+			wasBorderChanged = true;
+		}
+		internal void GainOccupation(Land province){
+			occupations.Add(province);
+		}
+		internal void LoseOccupation(Land province){
+			occupations.Remove(province);
+		}
+		#endregion
+		
+		#region Regenerating the Border Mesh
+		private void Update(){
+			if (wasBorderChanged){
+				RegenerateBorder();
+			}
+		}
+		private void RegenerateBorder(){
+			DestroyImmediate(borderMeshFilter.sharedMesh);
+			if (provinces.Count == 0){
+				return;
+			}
+			MeshData borderMeshData = new($"{gameObject.name}BorderMesh");
+			HashSet<Province> unsearchedProvinces = new(provinces.Select(land => land.Province));
+			CountryBorder.Generate(borderMeshData, this, unsearchedProvinces, borderHalfWidth);
+			borderMeshFilter.sharedMesh = borderMeshData.ToMesh();
+			wasBorderChanged = false;
+		}
+		
 		#endregion
 		
 		#region Selection Interface for the UI
@@ -238,9 +230,17 @@ namespace Simulation {
 		}
 		#endregion
 		
+		#region Getter Methods
+		public DiplomaticStatus GetDiplomaticStatus(Country other){
+			return map.GetDiplomaticStatus(this, other);
+		}
+		public PeaceTreaty NewPeaceTreaty(Country recipient){
+			return new PeaceTreaty(this, recipient, truceData);
+		}
 		public override string ToString(){
 			return Name;
 		}
+		#endregion
 	}
 
 	[Serializable]
