a8a672ad4a7a730a9cd30f3706d39139af72b452
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-04-14 19:21:33 +0200

    Refactored TickEvent and CallbackSorter

--- Assets/Scripts/Game/Simulation/Time/CallbackSorter.cs
+++ Assets/Scripts/Game/Simulation/Time/CallbackSorter.cs
@@ -20,23 +20,34 @@ namespace Simulation {
 		
 		public int Compare(Action left, Action right){
 			if (left == null){
-				return right == null ? 0 : 1;
+				return right == null ? 0 : +1;
 			}
 			if (right == null){
 				return -1;
 			}
-			if (left.Target == right.Target && left.Method == right.Method){
+			if (left.Equals(right)){
 				return 0;
 			}
-			// Types not in the priority Dictionary get lower priority than all Types in it.
-			if (ContainsKey(left) || ContainsKey(right)){
+			if (DoesNotContainKey(left)){
+				return +1;
+			}
+			if (DoesNotContainKey(right)){
 				return -1;
 			}
 			int result = GetPriority(left)-GetPriority(right);
-			// Equal priority does not count as equality so result isn't allowed to be 0.
-			return result == 0 ? 1 : result;
+			// Equal priority isn't enough to count as equality so the result isn't allowed to be 0.
+			if (result == 0){
+				return HashCode.Combine(left.Target, left.Method.MethodHandle.Value)-HashCode.Combine(right.Target, right.Method.MethodHandle.Value);
+			}
+			return result;
 		}
-		private bool ContainsKey(Action action) => action.Target == null || !priority.ContainsKey(action.Target.GetType());
+		private bool DoesNotContainKey(Action action){
+			Type type = action.Target?.GetType();
+			if (type == null){
+				return false;
+			}
+			return !priority.ContainsKey(type);
+		}
 		private int GetPriority(Action action) => priority[action.Target.GetType()];
 	}
 }
--- Assets/Scripts/Game/Simulation/Time/TickEvent.cs
+++ Assets/Scripts/Game/Simulation/Time/TickEvent.cs
@@ -5,18 +5,36 @@ namespace Simulation {
 	public class TickEvent {
 		private readonly SortedSet<Action> callbacks;
 		private readonly Queue<(Action, bool)> callbackChangeBuffer = new();
-
+		private bool isIterating;
+		
 		internal TickEvent(IComparer<Action> sorter){
 			callbacks = new SortedSet<Action>(sorter);
 		}
 		
 		public void AddListener(Action callback){
-			callbackChangeBuffer.Enqueue((callback, true));
+			if (isIterating){
+				callbackChangeBuffer.Enqueue((callback, true));
+			} else {
+				callbacks.Add(callback);
+			}
 		}
 		public void RemoveListener(Action callback){
-			callbackChangeBuffer.Enqueue((callback, false));
+			if (isIterating){
+				callbackChangeBuffer.Enqueue((callback, false));
+			} else {
+				callbacks.Remove(callback);
+			}
 		}
 		public void Invoke(){
+			GoThroughBuffer();
+			isIterating = true;
+			foreach (Action callback in callbacks){
+				callback();
+			}
+			isIterating = false;
+			GoThroughBuffer();
+		}
+		private void GoThroughBuffer(){
 			while (callbackChangeBuffer.Count > 0){
 				(Action callback, bool doAdd) = callbackChangeBuffer.Dequeue();
 				if (doAdd){
@@ -24,9 +42,6 @@ namespace Simulation {
 				} else {
 					callbacks.Remove(callback);
 				}
-			}
-			foreach (Action callback in callbacks){
-				callback();
 			}
 		}
 	}
