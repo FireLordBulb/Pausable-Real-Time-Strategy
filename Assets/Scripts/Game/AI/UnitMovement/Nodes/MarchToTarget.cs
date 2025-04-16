84d54407bc502f5c22641ac4c469177fbca5743c
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-04-16 11:20:29 +0200

    Made AI avoid walking through powerful enemy armies.

--- Assets/Scripts/Game/AI/UnitMovement/Nodes/MarchToTarget.cs
+++ Assets/Scripts/Game/AI/UnitMovement/Nodes/MarchToTarget.cs
@@ -7,20 +7,45 @@ namespace AI.Nodes {
 	[CreateAssetMenu(fileName = "MoveRegiment", menuName = "ScriptableObjects/AI/Nodes/MoveRegiment")]
 	public class MarchToTarget : MilitaryUnitNode<Regiment> {
 		private List<ProvinceLink> pathToTarget;
+		private Province nextProvince;
+		private int pathIndex;
 		protected override void OnStart(){
 			base.OnStart();
 			pathToTarget = Tree.Blackboard.GetValue(Brain.PathToTarget, new List<ProvinceLink>());
-			if (pathToTarget.Count == 0){
-				return;
-			}
-			MoveOrderResult result = Brain.Controller.Country.MoveRegimentTo(Brain.Unit, pathToTarget[^1].Target);
-			CurrentState = result == MoveOrderResult.Success ? State.Running : State.Failure;
+			pathIndex = -1;
+			nextProvince = Brain.Unit.Province;
 		}
 		protected override State OnUpdate(){
-			if (!Brain.Unit.IsMoving){
-				CurrentState = pathToTarget.Count == 0 || Brain.Unit.Province == pathToTarget[^1].Target ? State.Success : State.Failure;
-			}
+			UpdateCurrentState();
 			return CurrentState;
+		}
+		private void UpdateCurrentState(){
+			// Cancel the movement if a big scary army moved into the path.
+			if (Brain.Controller.ShouldAvoidArmyAt(nextProvince, Brain.Unit)){
+				Brain.Controller.Country.MoveRegimentTo(Brain.Unit, Brain.Unit.Province);
+				CurrentState = State.Failure;
+				return;
+			}
+			// Wait for the movement to progress.
+			if (Brain.Unit.IsMoving){
+				return;
+			}
+			// This case can only happen if something besides this node is giving move orders to the regiment, don't do that.
+			if (Brain.Unit.Province != nextProvince){
+				CurrentState = State.Failure;
+				return;
+			}
+			// Either start moving toward the next province in the path or conclude with State.Success if the path has ended.
+			pathIndex++;
+			if (pathIndex < pathToTarget.Count){
+				nextProvince = pathToTarget[pathIndex].Target;
+				MoveOrderResult result = Brain.Controller.Country.MoveRegimentTo(Brain.Unit, nextProvince);
+				if (result != MoveOrderResult.Success){
+					CurrentState = State.Failure;
+				}
+				return;
+			}
+			CurrentState = State.Success;
 		}
 	}
 }
