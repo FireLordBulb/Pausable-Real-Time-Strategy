c62b723a53523b757a179c479de5cd97854d8eb5
Vilgot Ottestig <vilgot.ottestig@gmail.com>
2025-05-01 19:35:41 +0200

    Made IncomeBreakdown sort items by largest absolute value.

--- Assets/Scripts/Game/Player/UIElements/IncomeBreakdownHover.cs
+++ Assets/Scripts/Game/Player/UIElements/IncomeBreakdownHover.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Simulation;
 using Text;
 using UnityEngine;
@@ -35,13 +36,13 @@ namespace Player {
             sourceColumn.Clear();
             switch (resource){
                 case Resource.Gold:
-                    AddRows(Player.GoldIncome, "Income", Player.MonthlyGoldChanges, FormatNumber, Add);
+                    AddRows(Player.GoldIncome, "Income", Player.MonthlyGoldChanges, FormatNumber, Add, Compare);
                     break;
                 case Resource.Manpower:
-                    AddRows(Player.ManpowerIncome, "Reserves", Player.MonthlyManpowerChanges, FormatNumber, Add);
+                    AddRows(Player.ManpowerIncome, "Reserves", Player.MonthlyManpowerChanges, FormatNumber, Add, Compare);
                     break;
                 case Resource.Sailors:
-                    AddRows(Player.SailorsIncome, "Reserves", Player.MonthlySailorsChanges, FormatNumber, Add);
+                    AddRows(Player.SailorsIncome, "Reserves", Player.MonthlySailorsChanges, FormatNumber, Add, Compare);
                     break;
                 default: return;
             }
@@ -49,8 +50,8 @@ namespace Player {
             breakdown.UpdateColumn(Value, valueColumn.ToArray());
         }
         
-        private void AddRows<T>(T income, string nameOfTotal, IReadOnlyList<(T, string, Type)> monthlyChanges, Func<T, string> formatter, Func<T, T, T> adder) where T : new() {
-            valueColumn.Add(Bold(formatter(income)));
+        private void AddRows<T>(T income, string nameOfTotal, IReadOnlyList<(T, string, Type)> monthlyChanges, Func<T, string> format, Func<T, T, T> add, Func<T, T, int> compare) where T : new() {
+            valueColumn.Add(Bold(format(income)));
             sourceColumn.Add(Bold($"Net Monthly {nameOfTotal}"));
             valueColumn.Add("-----------------------------------");
             sourceColumn.Add("");
@@ -65,37 +66,39 @@ namespace Player {
                 if (type != typeof(Land)){
                     break;
                 }
-                SumUpDuplicate(value, source, provinceDuplicates, adder);
-                provinceTotal = adder(provinceTotal, value);
+                SumUpDuplicate(value, source, provinceDuplicates, add);
+                provinceTotal = add(provinceTotal, value);
             }
-            AddDuplicates(provinceDuplicates, n => Indent(formatter(n)));
+            AddDuplicates(provinceDuplicates, n => Indent(format(n)), compare);
             
             Dictionary<string, (T, int)> otherDuplicates = new();
-            valueColumn[provinceTotalValueIndex] = formatter(provinceTotal);
+            valueColumn[provinceTotalValueIndex] = format(provinceTotal);
             for (; i < monthlyChanges.Count; i++){
                 (T value, string source, _) = monthlyChanges[i];
-                SumUpDuplicate(value, source, otherDuplicates, adder);
+                SumUpDuplicate(value, source, otherDuplicates, add);
             }
-            AddDuplicates(otherDuplicates, formatter);
+            AddDuplicates(otherDuplicates, format, compare);
         }
-        private static void SumUpDuplicate<T>(T value, string source, Dictionary<string, (T, int)> duplicates, Func<T, T, T> adder){
+        private static void SumUpDuplicate<T>(T value, string source, Dictionary<string, (T, int)> duplicates, Func<T, T, T> add){
             if (duplicates.TryGetValue(source, out (T total, int count) duplicate)){
-                duplicates[source] = (adder(duplicate.total, value), duplicate.count+1);
+                duplicates[source] = (add(duplicate.total, value), duplicate.count+1);
             } else{
                 duplicates[source] = (value, 1);
             }
         }
-        private void AddDuplicates<T>(Dictionary<string, (T, int)> duplicates, Func<T, string> formatter){
-            foreach ((string source, (T total, int count) duplicate) in duplicates){
-                valueColumn.Add(formatter(duplicate.total));
-                sourceColumn.Add($"{source}{(duplicate.count > 1 ? $" (\u00d7{duplicate.count})" : "")}");
+        private void AddDuplicates<T>(Dictionary<string, (T, int)> duplicates, Func<T, string> format, Func<T, T, int> compare){
+            KeyValuePair<string, (T total, int count)>[] duplicateArray = duplicates.ToArray();
+            Array.Sort(duplicateArray, (pairA, pairB) => compare(pairA.Value.total, pairB.Value.total));
+            foreach ((string source, (T total, int count) duplicate) in duplicateArray){
+                valueColumn.Add(format(duplicate.total));
+                sourceColumn.Add(duplicate.count > 1 ? $"{source} (\u00d7{duplicate.count})" : source);
             }
         }
         
-        private string Bold(string text){
+        private static string Bold(string text){
             return $"<b>{text}</b>";
         }
-        private string Indent(string text){
+        private static string Indent(string text){
             return $"  {text}";
         }
         private string FormatNumber(float value){
@@ -104,7 +107,9 @@ namespace Player {
         private string FormatNumber(int value){
             return Format.FormatLargeNumberWithSign(value, maxNumberCharacters);
         }
-        private static int Add(int a, int b) => a+b;
+        private static int Compare(float a, float b) => Mathf.CeilToInt(Mathf.Abs(b)-Mathf.Abs(a));
+        private static int Compare(int a, int b) => Mathf.Abs(b)-Mathf.Abs(a);
         private static float Add(float a, float b) => a+b;
+        private static int Add(int a, int b) => a+b;
     }
 }
