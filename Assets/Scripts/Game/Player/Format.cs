7ec91f57dd139472d74842ba67607e4a9d7d224c
Vilgot Ottestig <vilgot.ottestig@gmail.com>
2025-05-01 13:25:00 +0200

    Made Formatted large numbers display as many sig-figs as possible.

--- Assets/Scripts/Game/Player/Format.cs
+++ Assets/Scripts/Game/Player/Format.cs
@@ -1,9 +1,12 @@
 using System.Globalization;
+using System.Text;
 using UnityEngine;
 
 namespace Player {
 	public static class Format {
 		private const int SuffixDigitStep = 3;
+		private const int Base = 10;
+		private const decimal InverseBase = 1/(decimal)Base;
 		private static readonly (char, long)[] Suffixes ={
 			('k', 1000),
 			('M', 1000000),
@@ -13,37 +16,55 @@ namespace Player {
 			('E', 1000000000000000000)
 		};
 		private const float Cent = 100f;
-		
-		public static string FormatLargeNumber(float number, int maxCharacters){
-			string untruncated = FloatToString(number);
+
+		public static string FormatLargeNumber(double number, int maxCharacters){
+			return FormatLargeNumber((decimal)number, maxCharacters);
+		}
+		public static string FormatLargeNumber(decimal number, int maxCharacters){
+			// Removes the part of the number that is after the last visible digit. This is needed because Single.ToString rounds those values.
+			number -= number%InverseBase;
+			string untruncated = number.ToString("0.0", CultureInfo.InvariantCulture);
 			int characterAmount = untruncated.Length;
 			if (characterAmount <= maxCharacters){
 				return untruncated;
 			}
-			(char character, long value) = GetSuffix(characterAmount, maxCharacters);
-			return $"{FloatToString(number/value)}{character}";
+			((char character, long value), int emptyCharacterCount) = GetSuffix(characterAmount, maxCharacters);
+			StringBuilder format = new("0.");
+			// Display as many digits after the decimal as can fit.
+			decimal smallestSigFig = 1;
+			for (int i = 0; i < emptyCharacterCount; i++){
+				format.Append('0');
+				smallestSigFig *= InverseBase;
+			}
+			decimal numberSuffixScaled = number/value;
+			// See first comment in this method.
+			numberSuffixScaled -= numberSuffixScaled%smallestSigFig;
+			return $"{numberSuffixScaled.ToString(format.ToString(), CultureInfo.InvariantCulture)}{character}";
 		}
-		public static string FormatLargeNumber(int number, int maxCharacters){
+		public static string FormatLargeNumber(long number, int maxCharacters){
 			string untruncated = number.ToString();
 			int characterAmount = untruncated.Length;
 			if (characterAmount <= maxCharacters){
 				return untruncated;
 			}
-			(char character, long value) = GetSuffix(characterAmount, maxCharacters);
-			return $"{(number/value).ToString()}{character}";
+			((char character, long value), int emptyCharacterCount) = GetSuffix(characterAmount, maxCharacters);
+			StringBuilder formattedNumber = new((number/value).ToString());
+			// Sometimes it's possible to squeeze in one extra sig-fig.
+			if (emptyCharacterCount == SuffixDigitStep){
+				formattedNumber.Append($".{number*Base/value % Base}");
+			}
+			formattedNumber.Append(character);
+			return formattedNumber.ToString();
 		}
-		private static (char, long) GetSuffix(int characterAmount, int maxCharacters){
+		private static ((char, long), int) GetSuffix(int characterAmount, int maxCharacters){
 			foreach ((char, long) suffix in Suffixes){
 				characterAmount -= SuffixDigitStep;
 				if (characterAmount < maxCharacters){
-					return suffix;
+					return (suffix, maxCharacters-characterAmount);
 				}
 			}
 			// In case no suffix works just use the largest one.
-			return Suffixes[^1];
-		}
-		private static string FloatToString(float number){
-			return number.ToString("0.0", CultureInfo.InvariantCulture);
+			return (Suffixes[^1], 0);
 		}
 		
 		public static string SignedPercent(float value){
