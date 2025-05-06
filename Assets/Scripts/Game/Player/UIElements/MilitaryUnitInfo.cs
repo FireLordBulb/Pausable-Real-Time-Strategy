6a627321c6db02cd199e5236b98b8bf973444f21
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-05-06 14:49:23 +0200

    Made MilitaryUnitInfo show a unit's HpText & owning country's flag.

--- Assets/Scripts/Game/Player/UIElements/MilitaryUnitInfo.cs
+++ Assets/Scripts/Game/Player/UIElements/MilitaryUnitInfo.cs
@@ -1,13 +1,20 @@
 using Simulation.Military;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Player {
 	[RequireComponent(typeof(RectTransform))]
 	public class MilitaryUnitInfo : MonoBehaviour {
+		[SerializeField] private TextMeshProUGUI healthPoints;
+		[SerializeField] private Image flag;
 		public RectTransform RectTransform => (RectTransform)transform;
 		
 		public void Init(IUnit unit){
-			
+			healthPoints.text = unit.HpText;
+			flag.material = new Material(flag.material){
+				color = unit.Owner.MapColor
+			};
 		}
 	}
 }
