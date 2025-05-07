82aa3cd7b227155dc8b5b54be14afd6cafc775e4
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-05-07 13:52:30 +0200

    Made Transport window display a list of regiments on deck.

--- Assets/Scripts/Game/Player/UILayers/SelectionWindows/TransportWindow.cs
+++ Assets/Scripts/Game/Player/UILayers/SelectionWindows/TransportWindow.cs
@@ -2,9 +2,12 @@ using System.Globalization;
 using Simulation;
 using Simulation.Military;
 using Text;
+using UnityEngine;
 
 namespace Player {
 	public class TransportWindow : MilitaryUnitWindow<Transport> {
+		[Header("TransportDeck")]
+		[SerializeField] private UnitListScrollView unitListScrollView;
 		protected override void RefreshCombatTable(){
 			combatValuesTable.UpdateColumn(-1, (
 				Selected.HullText),
@@ -12,6 +15,7 @@ namespace Player {
 				Format.Percent(Selected.Size),
 				Selected.UsedManpowerCapacityText
 			);
+			unitListScrollView.Refresh(Selected.Deck.Units, UI);
 		}
 		
 		protected override void OrderMove(Province province){
