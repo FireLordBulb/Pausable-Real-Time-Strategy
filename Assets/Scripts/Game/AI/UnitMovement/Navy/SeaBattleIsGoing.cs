1ac41ec015fd550cba9cc95ff3ced71f00f92cbb
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2025-05-07 18:28:21 +0200

    Made SeaBattleIsGoing use SailTargetDecorator as its base class to remove redundant duplication.

--- Assets/Scripts/Game/AI/UnitMovement/Navy/SeaBattleIsGoing.cs
+++ Assets/Scripts/Game/AI/UnitMovement/Navy/SeaBattleIsGoing.cs
@@ -3,18 +3,9 @@ using UnityEngine;
 
 namespace AI.Nodes {
 	[CreateAssetMenu(fileName = "SeaBattleIsGoing", menuName = "ScriptableObjects/AI/Nodes/SeaBattleIsGoing")]
-	public class SeaBattleIsGoing : UnitDecorator<Ship> {
-		private Location<Ship> targetLocation;
-		
-		protected override void OnStart(){
-			base.OnStart();
-			targetLocation = Blackboard.GetValue<Location<Ship>>(Brain.Target, null);
-		}
-		protected override bool Predicate(){
-			return targetLocation != null && Brain.IsReinforceableBattleOngoing(targetLocation);
-		}
-		protected override void OnFailure(){
-			Country.MoveFleetTo(Unit, Unit.Location);
+	public class SeaBattleIsGoing : SailTargetDecorator {
+		protected override bool IsTargetValid(Location<Ship> targetLocation){
+			return Brain.IsReinforceableBattleOngoing(targetLocation);
 		}
 	}
 }
